fc1fc7759a2b9ec7e2cf2e6325dbe65f045bc557
Technius <bryan99tan@gmail.com>
2011-12-18 12:02:25 -0800

    Added an "API" style class

--- src/me/dbizzzle/SkyrimRPG/SpellManager.java
+++ src/me/dbizzzle/SkyrimRPG/SpellManager.java
@@ -4,11 +4,20 @@ import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-public class SpellManager {
-	public void shootFireball(Player shooter) {
+public class SpellManager 
+{
+	SkyrimRPG p;
+	public SpellManager(SkyrimRPG p)
+	{
+		this.p = p;
+	}
+	public void shootFireball(Player shooter, int multiplier)
+	{
 		final Vector direction = shooter.getEyeLocation().getDirection().multiply(2);
 		Fireball fireball = shooter.getWorld().spawn(shooter.getEyeLocation().add(direction.getX(), direction.getY(), 
 			direction.getZ()), Fireball.class);
 		fireball.setShooter(shooter);
+		fireball.setYield(4F*(multiplier/100));
+		p.log.info("" + Float.toString(multiplier/100));
 	}
 }
