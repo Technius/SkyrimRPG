675464587205d9ba487392cd3b938a11aedb1254
Daniel Boudreau <dbizzzle@hotmail.com>
2011-12-17 21:14:51 -0400

    First Commit

---
+++ src/me/dbizzzle/SkyrimRPG/SkyrimRPG.java
@@ -0,0 +1,17 @@
+package me.dbizzzle.SkyrimRPG;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkyrimRPG extends JavaPlugin {
+	public Logger log = Logger.getLogger("Minecraft");
+	
+	public void onEnable() {
+		log.info("[SkyrimRPG] Plugin enabled.");
+	}
+	
+	public void onDisable() {
+		log.info("[SkyrimRPG] Plugin disabled.");
+	}
+}
