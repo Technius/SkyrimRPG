d87e6219fc62727abf7d82f368350a097a2b0102
Technius <bryan99tan@gmail.com>
2011-12-18 08:01:37 -0800

    Added a SpellManager class for future use

---
+++ src/me/dbizzzle/SkyrimRPG/SpellManager.java
@@ -0,0 +1,16 @@
+package me.dbizzzle.SkyrimRPG;
+
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+public class SpellManager 
+{
+	public void shootFireball(Player shooter)
+	{
+		final Vector direction = shooter.getEyeLocation().getDirection().multiply(2);
+		Fireball fireball = shooter.getWorld().spawn(shooter.getEyeLocation().add(direction.getX(), direction.getY(), 
+			direction.getZ()), Fireball.class);
+		fireball.setShooter(shooter);
+	}
+}
