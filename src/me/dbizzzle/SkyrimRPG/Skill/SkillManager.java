520421d78385c73da3b8b1d7512b0ee65f6a5ac5
Technius <bryan99tan@gmail.com>
2011-12-18 19:06:16 -0800

    Increment actually increments now

--- src/me/dbizzzle/SkyrimRPG/Skill/SkillManager.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/SkillManager.java
@@ -7,14 +7,16 @@ import org.bukkit.entity.Player;
 public class SkillManager 
 {
 	public static HashMap<Player, HashMap<String, Integer>> skills = new HashMap<Player, HashMap<String, Integer>>();
+	
 	public static HashMap<String, Integer> getSkills (Player player)
 	{
 		return skills.get(player);
 	}
 	public void incrementLevel(String skill, Player player)
 	{
-		int l = skills.get(player).get(skill).intValue();
+		int l = skills.get(player).get(skill).intValue() + 1;
 		skills.get(player).put(skill, Integer.valueOf(l));
+		player.sendMessage(skill + " increased to level " + l);
 	}
 	public void loadSkills(Player player)
 	{
