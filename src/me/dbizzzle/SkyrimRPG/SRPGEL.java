52bab1d4cad2f2688b9f0cd0b6f1de8101cb76c7
Technius <bryan99tan@gmail.com>
2011-12-18 17:11:52 -0800

    Added Archery

---
+++ src/me/dbizzzle/SkyrimRPG/SRPGEL.java
@@ -0,0 +1,33 @@
+package me.dbizzzle.SkyrimRPG;
+
+import me.dbizzzle.SkyrimRPG.Skill.SkillManager;
+
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityListener;
+
+public class SRPGEL extends EntityListener
+{
+	public void onEntityDamage(EntityDamageEvent event)
+	{
+		if(event instanceof EntityDamageByEntityEvent)
+		{
+			EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
+			if(!(e.getDamager() instanceof Arrow))return;
+			Arrow a = (Arrow)e.getDamager();
+			if(a.getShooter() instanceof Player)
+			{
+				Player shooter = (Player)a.getServer();
+				int alevel = SkillManager.getSkills(shooter).get("Archery");
+				e.setDamage(e.getDamage() + (alevel/10));
+			}
+			else return;
+		}
+		else
+		{
+			
+		}
+	}
+}
