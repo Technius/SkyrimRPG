4dbcb1baf63211268ad2b481f0d2a64cc0ba6fd7
Technius <bryan99tan@gmail.com>
2011-12-20 08:23:24 -0800

    Added fireballs to a tracker

--- src/me/dbizzzle/SkyrimRPG/SpellManager.java
+++ src/me/dbizzzle/SkyrimRPG/SpellManager.java
@@ -1,11 +1,15 @@
 package me.dbizzzle.SkyrimRPG;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class SpellManager 
 {
+	public static List<Fireball>ftracker = new ArrayList<Fireball>();
 	SkyrimRPG p;
 	public SpellManager(SkyrimRPG p)
 	{
@@ -18,5 +22,6 @@ public class SpellManager
 			direction.getZ()), Fireball.class);
 		fireball.setShooter(shooter);
 		fireball.setYield(4F*(multiplier/100));
+		ftracker.add(fireball);
 	}
 }
