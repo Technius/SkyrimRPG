f3c56c11a02ac8762d54d72d02f0d6f6610028ce
Technius <bryan99tan@gmail.com>
2011-12-20 22:18:40 -0800

    Added the enum Level to LockManager

--- src/me/dbizzzle/SkyrimRPG/Skill/LockManager.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/LockManager.java
@@ -1,5 +1,6 @@
 package me.dbizzzle.SkyrimRPG.Skill;
 
+import java.io.File;
 import java.util.HashMap;
 
 import org.bukkit.block.Block;
@@ -9,7 +10,7 @@ import me.dbizzzle.SkyrimRPG.SkyrimRPG;
 
 public class LockManager 
 {
-	public static HashMap<Player, Block> locklist = new HashMap<Player,Block>();
+	public static HashMap<Player, HashMap<Block,Integer>> locklist = new HashMap<Player,HashMap<Block,Integer>>();
 	SkyrimRPG r;
 	public LockManager(SkyrimRPG r)
 	{
@@ -17,6 +18,24 @@ public class LockManager
 	}
 	public void loadLocks()
 	{
+		File locks = new File(r.getDataFolder().getPath() + File.separator + "Locks");
+		if(!locks.exists())
+		{
+			locks.mkdir();
+			return;
+		}
+	}
+	public void addLock(Block b, Player owner, int level)
+	{
 		
+	}
+	public enum Level
+	{
+		NOVICE(1),APPRENTICE(2),ADEPT(3),EXPERT(4),MASTER(5);
+		private int level;
+		private Level(int c)
+		{
+			level = c;
+		}
 	}
 }
