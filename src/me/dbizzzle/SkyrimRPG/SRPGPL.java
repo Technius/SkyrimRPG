77ec3b72aad8ee1699d3e2873a2ebde4bab9e203
Technius <bryan99tan@gmail.com>
2011-12-21 15:22:02 -0800

    Fixed spacing

--- src/me/dbizzzle/SkyrimRPG/SRPGPL.java
+++ src/me/dbizzzle/SkyrimRPG/SRPGPL.java
@@ -22,139 +22,139 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class SRPGPL extends PlayerListener {
-public SkyrimRPG plugin;
+	public SkyrimRPG plugin;
 
-public SRPGPL(SkyrimRPG p) {
-plugin = p;
-}
+	public SRPGPL(SkyrimRPG p) {
+		plugin = p;
+	}
 
-int secondsDelay = 20; //This will be configurable, I just set 20 for now
-long delay = secondsDelay*20;
+	int secondsDelay = 20; //This will be configurable, I just set 20 for now
+	long delay = secondsDelay*20;
 
-String pickpocketed = ChatColor.RED + "Somebody has pickpocketed you!"; //Configurable
+	String pickpocketed = ChatColor.RED + "Somebody has pickpocketed you!"; //Configurable
 
-public void onPlayerInteract(PlayerInteractEvent event)
-{
-if(event.getPlayer().getItemInHand().getType() == Material.BLAZE_ROD)
-{
-if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
-{
-plugin.st.chargeFireball(event.getPlayer());
-event.getPlayer().sendMessage("Charging fireball...");
-}
-else if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
-{
-int m = plugin.st.unchargeFireball(event.getPlayer());
-if(m == -1) return;
-plugin.sm.shootFireball(event.getPlayer(), m);
-event.getPlayer().sendMessage("Fireball shot!");
-}
-}
-else if (event.getPlayer().getItemInHand().getType() == Material.REDSTONE_TORCH_ON)
-{
-if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
-{
-if (event.getClickedBlock().getType() == Material.IRON_DOOR)
-{
-Player s = event.getPlayer();
-Inventory inv = s.getInventory();
-if (inv.contains(Material.IRON_INGOT))
-{
-if (pickLockSuccess(s, s.getEyeLocation()))
-{
-SkillManager sm = new SkillManager();
-if (sm.processExperience(s, "Lockpicking"))
-{
-sm.incrementLevel("Lockpicking", s);
-SkillManager.progress.get(s).put("Lockpicking", 0);
-}
-else SkillManager.progress.get(s).put("Lockpicking", SkillManager.progress.get(s).get("Lockpicking") + 1);
-s.sendMessage(ChatColor.GREEN + "You picked the lock successfully!");
-}
-else
-{
-int alevel = SkillManager.getSkillLevel("Lockpicking", s);
-Random r = new Random();
-int calc = 20 - (alevel/10);
-if(r.nextInt(calc + 1) < alevel/10)
-{
-s.sendMessage(ChatColor.RED + "You failed at picking the lock, and one of your lockpicks broke!");
-for(ItemStack i:inv.getContents())
-{
-if(i.getType() != Material.IRON_INGOT)continue;
-i.setAmount(i.getAmount() - 1);
-break;
-}
-}
-else s.sendMessage(ChatColor.RED + "You failed at picking the lock!");
-}
-}
-}
-}
-}
-}
-
-public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-Player se = event.getPlayer();
-EntityPlayer s = ((CraftPlayer) event.getPlayer()).getHandle();
-if (s.isSneaking()) {
-Entity ent = event.getRightClicked();
-final String ents = ((HumanEntity) ent).getName();
-if (ent instanceof Player) {
-EntityPlayer pick = ((CraftPlayer) plugin.getServer().getPlayer(ents)).getHandle();
-s.a(pick.inventory);
-se.sendMessage(ChatColor.GREEN + "You have succesfully pickpocketed " + ents + "!");
-
-SkillManager sm = new SkillManager();
-if (sm.processExperience(se, "PickPocket")) {
-sm.incrementLevel("PickPocket", se);
-SkillManager.progress.get(se).put("PickPocket", 0);
-} else {
-SkillManager.progress.get(se).put("PickPocket", SkillManager.progress.get(se).get("PickPocket") + 1);
+	public void onPlayerInteract(PlayerInteractEvent event)
+	{
+		if(event.getPlayer().getItemInHand().getType() == Material.BLAZE_ROD)
+		{
+			if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
+			{
+				plugin.st.chargeFireball(event.getPlayer());
+				event.getPlayer().sendMessage("Charging fireball...");
+			}	
+			else if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+			{
+				int m = plugin.st.unchargeFireball(event.getPlayer());
+				if(m == -1) return;
+				plugin.sm.shootFireball(event.getPlayer(), m);
+				event.getPlayer().sendMessage("Fireball shot!");
+			}
+		}
+		else if (event.getPlayer().getItemInHand().getType() == Material.REDSTONE_TORCH_ON)
+		{
+			if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
+			{
+				if (event.getClickedBlock().getType() == Material.IRON_DOOR)
+				{
+					Player s = event.getPlayer();
+					Inventory inv = s.getInventory();
+					if (inv.contains(Material.IRON_INGOT))
+					{
+						if (pickLockSuccess(s, s.getEyeLocation()))
+						{
+							SkillManager sm = new SkillManager();
+							if (sm.processExperience(s, "Lockpicking"))
+							{
+								sm.incrementLevel("Lockpicking", s);
+								SkillManager.progress.get(s).put("Lockpicking", 0);
+							}
+							else SkillManager.progress.get(s).put("Lockpicking", SkillManager.progress.get(s).get("Lockpicking") + 1);
+							s.sendMessage(ChatColor.GREEN + "You picked the lock successfully!");
+						}
+						else
+						{
+							int alevel = SkillManager.getSkillLevel("Lockpicking", s);
+							Random r = new Random();
+							int calc = 20 - (alevel/10);
+							if(r.nextInt(calc + 1) < alevel/10)
+							{
+								s.sendMessage(ChatColor.RED + "You failed at picking the lock, and one of your lockpicks broke!");
+								for(ItemStack i:inv.getContents())
+								{
+									if(i.getType() != Material.IRON_INGOT)continue;
+									i.setAmount(i.getAmount() - 1);
+									break;
+								}
+							}
+							else s.sendMessage(ChatColor.RED + "You failed at picking the lock!");
+						}
+					}
+				}
+			}
+		}
 }
 
-plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-@SuppressWarnings("deprecation")
-public void run() {
-Player picked = plugin.getServer().getPlayer(ents);
-if(!picked.isOnline())return;
-picked.sendMessage(pickpocketed);
-picked.updateInventory();
-}
-}, delay);
-}
-}
-}
-public void onPlayerJoin(PlayerJoinEvent event)
-{
-SkillManager sm = new SkillManager();
-sm.setPlugin(plugin);
-sm.loadSkills(event.getPlayer());
-}
-public void onPlayerQuit(PlayerQuitEvent event)
-{
-SkillManager sm = new SkillManager();
-sm.setPlugin(plugin);
-sm.saveSkills(event.getPlayer());
-}
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+		Player se = event.getPlayer();
+		EntityPlayer s = ((CraftPlayer) event.getPlayer()).getHandle();
+		if (s.isSneaking()) {
+			Entity ent = event.getRightClicked();
+			final String ents = ((HumanEntity) ent).getName();
+			if (ent instanceof Player) {
+				EntityPlayer pick = ((CraftPlayer) plugin.getServer().getPlayer(ents)).getHandle();
+				s.a(pick.inventory);
+				se.sendMessage(ChatColor.GREEN + "You have succesfully pickpocketed " + ents + "!");
 
-public boolean pickLockSuccess(Player pla, Location loc)
-{
-if (loc.getBlock().getType() == Material.IRON_DOOR || loc.getBlock().getType() == Material.CHEST)
-{
-int alevel = SkillManager.getSkillLevel("LockPick", pla);
-Random r = new Random();
-int calc = 10 - (alevel/10);
-if (r.nextInt(calc + 1) < alevel/10)
-{
-return true;
-}
-else
-{
-return false;
-}
-}
-return false;
-}
+				SkillManager sm = new SkillManager();
+				if (sm.processExperience(se, "PickPocket")) {
+					sm.incrementLevel("PickPocket", se);
+					SkillManager.progress.get(se).put("PickPocket", 0);
+				} else {
+					SkillManager.progress.get(se).put("PickPocket", SkillManager.progress.get(se).get("PickPocket") + 1);
+				}
+
+				plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					@SuppressWarnings("deprecation")
+					public void run() {
+						Player picked = plugin.getServer().getPlayer(ents);
+						if(!picked.isOnline())return;
+						picked.sendMessage(pickpocketed);
+						picked.updateInventory();
+					}
+				}, delay);
+			}
+		}
+	}
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		SkillManager sm = new SkillManager();
+		sm.setPlugin(plugin);
+		sm.loadSkills(event.getPlayer());
+	}
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+		SkillManager sm = new SkillManager();
+		sm.setPlugin(plugin);
+		sm.saveSkills(event.getPlayer());
+	}
+
+	public boolean pickLockSuccess(Player pla, Location loc)
+	{
+		if (loc.getBlock().getType() == Material.IRON_DOOR || loc.getBlock().getType() == Material.CHEST)
+		{
+			int alevel = SkillManager.getSkillLevel("LockPick", pla);
+			Random r = new Random();
+			int calc = 10 - (alevel/10);
+			if (r.nextInt(calc + 1) < alevel/10)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
+	}
 }
 
