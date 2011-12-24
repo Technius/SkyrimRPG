b4689cf881da78fbbdfb67a2d55add1a44f86744
Technius <bryan99tan@gmail.com>
2011-12-24 12:07:10 -0800

    Fixed a small timer glitch

--- src/me/dbizzzle/SkyrimRPG/MagickaTimer.java
+++ src/me/dbizzzle/SkyrimRPG/MagickaTimer.java
@@ -15,8 +15,8 @@ public class MagickaTimer implements Runnable
 			int cl = clevel/10;
 			int dl = dlevel/10;
 			int newv = 5 + cl + dl + SpellManager.magicka.get(p);
-			if(newv > 100) continue;
-			SpellManager.magicka.put(p, newv);
+			if(newv > 100) SpellManager.magicka.put(p, 100);
+			else SpellManager.magicka.put(p, newv);
 		}
 	}
 
