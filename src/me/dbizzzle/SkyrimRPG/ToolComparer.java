385adbe937eaf3d4f985ae0998a80030441c3210
Technius <bryan99tan@gmail.com>
2011-12-27 19:55:10 -0800

    Added Axe

--- src/me/dbizzzle/SkyrimRPG/ToolComparer.java
+++ src/me/dbizzzle/SkyrimRPG/ToolComparer.java
@@ -9,6 +9,7 @@ public final class ToolComparer
 	{
 		Material tt = tool.getType();
 		if(tt==Material.IRON_SWORD||tt==Material.WOOD_SWORD||tt==Material.STONE_SWORD||tt==Material.GOLD_SWORD||tt==Material.DIAMOND_SWORD)return "Sword";
+		else if(tt==Material.IRON_AXE||tt==Material.WOOD_AXE||tt==Material.STONE_AXE||tt==Material.GOLD_AXE||tt==Material.DIAMOND_AXE)return "Axe";
 		else return null;
 	}
 }
