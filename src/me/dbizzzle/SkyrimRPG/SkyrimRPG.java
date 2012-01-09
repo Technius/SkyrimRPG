029b891820a2fd4157806ff7a9e03db0052d9514
Juan Mojica <thatbox@ymail.com>
2012-01-08 21:24:34 -0800

    Test.

--- src/me/dbizzzle/SkyrimRPG/SkyrimRPG.java
+++ src/me/dbizzzle/SkyrimRPG/SkyrimRPG.java
@@ -18,6 +18,8 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+// IGNORE.
+
 public class SkyrimRPG extends JavaPlugin 
 {
 	public Logger log = Logger.getLogger("Minecraft");
