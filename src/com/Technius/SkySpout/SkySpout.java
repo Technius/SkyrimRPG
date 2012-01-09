04d2c43e94db52c028f92ea0c8078d6351bda6e3
Technius <bryan99tan@gmail.com>
2012-01-08 21:51:18 -0800

    Updated

--- src/com/Technius/SkySpout/SkySpout.java
+++ src/com/Technius/SkySpout/SkySpout.java
@@ -1,15 +1,31 @@
 package com.Technius.SkySpout;
 
+import me.dbizzzle.SkyrimRPG.SkyrimRPG;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
 public class SkySpout 
 {
+	private SkyrimRPG p;
+	public SkySpout(SkyrimRPG p)
+	{
+		this.p = p;
+	}
+	private boolean useSpout = false;
 	Plugin spoutraw = null;
 	public boolean checkSpout(PluginManager pm)
 	{
 		spoutraw = pm.getPlugin("Spout");
-		if(spoutraw!= null)return true;
+		if(spoutraw!= null)
+		{
+			useSpout = true;
+			return true;
+		}
 		else return false;
+	}
+	public boolean useSpout()
+	{
+		return useSpout;
 	}
 }
