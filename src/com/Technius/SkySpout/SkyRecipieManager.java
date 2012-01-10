a901de28b63dbaeceb2038190704622b2106efb9
Technius <bryan99tan@gmail.com>
2012-01-09 18:31:13 -0800

    Some temporary crap,will fix later

--- src/com/Technius/SkySpout/SkyRecipieManager.java
+++ src/com/Technius/SkySpout/SkyRecipieManager.java
@@ -3,6 +3,7 @@ package com.Technius.SkySpout;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 
 import org.bukkit.inventory.ItemStack;
 import org.getspout.spoutapi.Spout;
@@ -11,6 +12,7 @@ import org.getspout.spoutapi.inventory.SpoutItemStack;
 import org.getspout.spoutapi.inventory.SpoutShapedRecipe;
 import org.getspout.spoutapi.material.MaterialData;
 
+import com.Technius.SkySpout.Items.DestructionSpellbook;
 import com.Technius.SkySpout.Items.Spellbook;
 
 import me.dbizzzle.SkyrimRPG.SkyrimRPG;
@@ -24,23 +26,21 @@ public class SkyRecipieManager
 	}
 	public void setupRecipies()
 	{
-		ItemStack spellbook = new SpoutItemStack(new Spellbook(p, null, null), 8);
-		SpoutShapedRecipe chainRecipe = new SpoutShapedRecipe(spellbook);
-		chainRecipe.shape(" B ", "B B", " B ");
-		chainRecipe.setIngredient('B', MaterialData.ironIngot);
-		SpoutManager.getMaterialManager().registerSpoutRecipe(chainRecipe);
+		ItemStack spellbook = new SpoutItemStack(new Spellbook(p, "DestructionSpellbook", null), 8);
+		SpoutShapedRecipe spellbookr = new SpoutShapedRecipe(spellbook);
+		spellbookr.shape(" B ", "BSB", " B ");
+		spellbookr.setIngredient('B', MaterialData.blazePowder);
+		spellbookr.setIngredient('S', MaterialData.book);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(spellbookr);
 	}
-	public File getSpellbookTexture()
+	public File getDestructionSpellbookTexture()
 	{
 		File tex = null;
-		try
-		{
-			InputStream is = SkyRecipieManager.class.getResourceAsStream("textures" + File.separator + "spellbook.png");
-			is.read();
-		}
-		catch(IOException ioe)
-		{
-			
+		try {
+			tex = new File(DestructionSpellbook.class.getResource("textures/DestructionSpellbook.png").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return tex;
 	}
