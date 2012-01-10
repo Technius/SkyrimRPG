2db9a281bf5902b082f3c7533d77436e96b2035b
Technius <bryan99tan@gmail.com>
2012-01-09 18:16:16 -0800

    Added a destruction spellbook texture

---
+++ src/com/Technius/SkySpout/Items/DestructionSpellbook.java
@@ -0,0 +1,12 @@
+package com.Technius.SkySpout.Items;
+
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.material.item.GenericCustomItem;
+
+public class DestructionSpellbook extends GenericCustomItem
+{
+	public DestructionSpellbook(Plugin plugin, String name, String texture) 
+	{
+		super(plugin, name, texture);
+	}
+}
