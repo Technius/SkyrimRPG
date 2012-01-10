9452f450ce7370acb826e6f093862faf5e310cdf
Juan Mojica <thatbox@ymail.com>
2012-01-09 21:51:10 -0800

    Fixed imports.

--- src/com/Technius/SkySpout/SkyRecipieManager.java
+++ src/com/Technius/SkySpout/SkyRecipieManager.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.bukkit.inventory.ItemStack;
-import org.getspout.spout.Spout;
+import org.getspout.spoutapi.Spout;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
 import org.getspout.spoutapi.inventory.SpoutShapedRecipe;
