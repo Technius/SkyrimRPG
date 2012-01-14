aa18233a15d409f3b13bf30382520fcc1cfc1589
Technius <bryan99tan@gmail.com>
2012-01-13 16:24:04 -0800

    Added getDisplayName to Spell

--- src/me/dbizzzle/SkyrimRPG/SpellManager.java
+++ src/me/dbizzzle/SkyrimRPG/SpellManager.java
@@ -127,6 +127,18 @@ public class SpellManager
 			}
 			return null;
 		}
+		public String getDisplayName()
+		{
+			String sl = toString().toLowerCase().replaceAll("_", " ");
+			String[] st = sl.split("[ ]");
+			String m = Character.toUpperCase(st[0].charAt(0)) + st[0].substring(1);
+			for(String x: st)
+			{
+				if(x.equalsIgnoreCase(st[0]))continue;
+				m = m + " " + Character.toUpperCase(x.charAt(0)) + x.substring(1);
+			}
+			return m;
+		}
 	}
 	public boolean hasSpell(Player player, Spell spell)
 	{
