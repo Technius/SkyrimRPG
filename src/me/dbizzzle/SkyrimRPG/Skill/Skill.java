eb2e4f3c8a703410521a5528d5eeddb75b1b3232
Technius <bryan99tan@gmail.com>
2012-01-15 11:26:13 -0800

    Removed Skill and Archery

--- src/me/dbizzzle/SkyrimRPG/Skill/Skill.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/Skill.java
@@ -1,23 +1,8 @@
 package me.dbizzzle.SkyrimRPG.Skill;
 
-public abstract class Skill 
+public enum Skill 
 {
-	public abstract Skill getType();
-	public abstract int getLevel();
-	public static final int MAX_LEVEL = 100;
-	public static final int BATTLE = 1;
-	public static final int MAGIC = 2;
-	public static final int STEALTH = 3;
-	public enum Battle
-	{
-		SWORDSMANSHIP, ARCHERY, AXECRAFT;
-	}
-	public enum Magic
-	{
-		DESTRUCTION,CONJURATION;
-	}
-	public enum Stealth
-	{
-		PICKPOCKET,LOCKPICKING;
-	}
+	ARCHERY, SWORDSMANSHIP, AXECRAFT, PICKPOCKETING, 
+	LOCKPICKING, BLOCKING,
+	CONJURATION, DESTRUCTION;
 }
