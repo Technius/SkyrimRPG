0dcb926be397e5732dbe7095d668d855f7b350c1
Technius <bryan99tan@gmail.com>
2012-01-16 08:06:47 -0800

    Finished savePerks and loadPerks

--- src/me/dbizzzle/SkyrimRPG/Skill/PerkManager.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/PerkManager.java
@@ -1,8 +1,11 @@
 package me.dbizzzle.SkyrimRPG.Skill;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -28,7 +31,35 @@ public class PerkManager
 		{
 			try
 			{
+				HashMap<Perk, Integer> pt = new HashMap<Perk, Integer>();
 				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream (plist)));
+				String l;
+				while((l=br.readLine())!= null)
+				{
+					if(l.startsWith("#"))continue;
+					String[] tokens = l.replaceAll(" ", "").split("[,]", 2);
+					if(tokens.length != 2)continue;
+					Perk p;
+					int level;
+					try
+					{
+						p = Perk.valueOf(tokens[0].toUpperCase());
+					}
+					catch(IllegalArgumentException iae)
+					{
+						continue;
+					}
+					try
+					{
+						level = Integer.parseInt(tokens[1]);
+					}
+					catch(NumberFormatException nfe)
+					{
+						continue;
+					}
+					pt.put(p, level);
+				}
+				PerkManager.perks.put(player, pt);
 			}
 			catch(IOException ioe)
 			{
@@ -38,12 +69,38 @@ public class PerkManager
 	}
 	public void savePerks(Player player)
 	{
-		
+		File file = new File(p.getDataFolder().getPath());
+		if(!file.exists())file.mkdir();
+		File perks = new File(file.getPath() + File.separator + "Perks");
+		if(!perks.exists())perks.mkdir();
+		File plist = new File(perks.getPath() + File.separator + player.getName() + ".txt");
+		try
+		{
+			if(!plist.exists())
+			{
+				FileOutputStream fos = new FileOutputStream(plist);
+				fos.flush();
+				fos.close();
+			}
+			BufferedWriter bw = new BufferedWriter(new FileWriter(plist));
+			bw.write("#Don't edit this unless you like errors");
+			bw.newLine();
+			for(Perk p:PerkManager.perks.get(player).keySet())
+			{
+				bw.write(p.toString() + PerkManager.perks.get(player).get(p));
+				bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+		}
+		catch(IOException ioe)
+		{
+			p.log.severe("[SkyrimRPG]COULD NOT SAVE \"" + player.getName() + "\"'s PERKS!!!");
+		}
 	}
 	public void defaultPerks(Player player)
 	{
 		HashMap<Perk, Integer> pt = new HashMap<Perk, Integer>();
-		pt.put(Perk.OVERDRAW, 0);
 		perks.put(player, pt);
 	}
 }
