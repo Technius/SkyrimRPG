c3894154dadd02be74fee4329046ba0e92cfb2f3
Technius <bryan99tan@gmail.com>
2012-01-16 18:25:36 -0800

    Added javadoc comments and required perk

--- src/me/dbizzzle/SkyrimRPG/Skill/Perk.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/Perk.java
@@ -2,28 +2,46 @@ package me.dbizzzle.SkyrimRPG.Skill;
 
 public enum Perk 
 {
-	OVERDRAW(Skill.ARCHERY, 5, 20, 40, 60, 80, 100);
+	OVERDRAW(Skill.ARCHERY, 5, null, 20, 40, 60, 80, 100);
 	private Skill parent;
 	private int maxlevel;
 	private int[] reqskilllvl;
-	private Perk(Skill parent, int maxlevel, int... rsl)
+	private Perk requisite;
+	private Perk(Skill parent, int maxlevel, Perk required, int... rsl)
 	{
 		this.maxlevel = maxlevel;
 		this.parent = parent;
 		reqskilllvl = rsl;
+		requisite = required;
 	}
+	/**
+	 * Returns the max level of the perk
+	 * @return
+	 */
 	public int getMaxLevel()
 	{
 		return maxlevel;
 	}
+	/**
+	 * Returns the skill this perk belongs to
+	 * @return The "parent" skill
+	 */
 	public Skill getSkill()
 	{
 		return parent;
 	}
+	/**
+	 * Returns an array of the required skill levels
+	 * @return An array of the required skill levels
+	 */
 	public int[] getRequiredSkillLevels()
 	{
 		return reqskilllvl;
 	}
+	/**
+	 * Returns a "nice", user friendly version of the name
+	 * @return The "nice" name
+	 */
 	public String getName()
 	{
 		String sl = toString().toLowerCase().replaceAll("_", " ");
@@ -35,5 +53,13 @@ public enum Perk
 			m = m + " " + Character.toUpperCase(x.charAt(0)) + x.substring(1);
 		}
 		return m;
+	}
+	/**
+	 * Returns a perk if it has a requisite
+	 * @return Perk if it has a requisite, otherwise null
+	 */
+	public Perk getRequiedPerk()
+	{
+		return requisite;
 	}
 }
