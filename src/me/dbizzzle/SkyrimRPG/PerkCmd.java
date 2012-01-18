4e12691e98424b90964730b769e3b3e503e727dc
Technius <bryan99tan@gmail.com>
2012-01-18 15:16:18 -0800

    Finished /addperk and started work on /perk

--- src/me/dbizzzle/SkyrimRPG/PerkCmd.java
+++ src/me/dbizzzle/SkyrimRPG/PerkCmd.java
@@ -22,7 +22,13 @@ public class PerkCmd implements CommandExecutor
 		if(sender instanceof Player)player = (Player)sender;
 		if(command.getName().equalsIgnoreCase("perk"))
 		{
-			
+			if(player == null)
+			{
+				plugin.log.info("You don't have perks!");
+				return true;
+			}
+			player.sendMessage(ChatColor.GOLD + "Perk Menu");
+			if(player.hasPermission("skyrimrpg.addperk"))player.sendMessage(ChatColor.GREEN + "/addperk [player] <perk> <level> " + ChatColor.RED + "- Adds a perk to specified player");
 		}
 		else if(command.getName().equalsIgnoreCase("addperk"))
 		{
@@ -60,18 +66,90 @@ public class PerkCmd implements CommandExecutor
 							return true;
 						}
 						PerkManager.perks.get(player).put(p, l);
-						player.sendMessage(ChatColor.GREEN + "Overdraw successfully added at level " + l);
+						player.sendMessage(ChatColor.GREEN + p.getName() + " successfully added at level " + l);
 					}
 				}
 				else
 				{
-					sender.sendMessage("Usage: /addperk <player> <perk> <level>");
+					plugin.log.info("Usage: /addperk <player> <perk> <level>");
 				}
 				return true;
 			case 3:
+				if(player == null)
+				{
+					Player t = plugin.getServer().getPlayer(args[0]);
+					if(t == null)
+					{
+						plugin.log.info("No such player!");
+						return true;
+					}
+					Perk p;
+					try
+					{
+						p = Perk.valueOf(args[1].toUpperCase());
+					}
+					catch(IllegalArgumentException iae)
+					{
+						sender.sendMessage("No such perk!");
+						return true;
+					}
+					int l;
+					try
+					{
+						l = Integer.parseInt(args[2]);
+						if(l>p.getMaxLevel())
+						{
+							sender.sendMessage(p.getName() + " has a max level of " + p.getMaxLevel() +", you entered " + l + "!");
+							return true;
+						}
+					}
+					catch(NumberFormatException nfe)
+					{
+						sender.sendMessage("That is not a valid number.");
+						return true;
+					}
+					PerkManager.perks.get(player).put(p, l);
+					sender.sendMessage(p.getName() + " successfully added at level " + l);
+				}
+				else if(player.hasPermission("skyrimrpg.addperk"))
+				{
+					Player t = plugin.getServer().getPlayer(args[0]);
+					if(t == null)
+					{
+						player.sendMessage(ChatColor.RED + "No such player!");
+						return true;
+					}
+					Perk p;
+					try
+					{
+						p = Perk.valueOf(args[1].toUpperCase());
+					}
+					catch(IllegalArgumentException iae)
+					{
+						player.sendMessage(ChatColor.RED + "No such perk!");
+						return true;
+					}
+					int l;
+					try
+					{
+						l = Integer.parseInt(args[2]);
+						if(l>p.getMaxLevel())
+						{
+							player.sendMessage(ChatColor.RED + p.getName() + " has a max level of " + p.getMaxLevel() +", you entered " + l + "!");
+							return true;
+						}
+					}
+					catch(NumberFormatException nfe)
+					{
+						player.sendMessage(ChatColor.RED + "That is not a valid number.");
+						return true;
+					}
+					PerkManager.perks.get(player).put(p, l);
+					player.sendMessage(ChatColor.RED + p.getName() + " successfully added at level " + l);
+				}
 				return true;
 			default:
-				if(player == null)sender.sendMessage("Usage: /addperk <player> <perk> <level>");
+				if(player == null)plugin.log.info("Usage: /addperk <player> <perk> <level>");
 				else if(player.hasPermission("skyrimrpg.addperk"))
 				{
 					player.sendMessage(ChatColor.RED + "Usage: /addperk <player> <perk> <level>");
