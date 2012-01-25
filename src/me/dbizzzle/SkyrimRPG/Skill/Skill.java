76b7c332635c5f005fbdd7b6509c8219018adbfd
Technius <bryan99tan@gmail.com>
2012-01-24 21:55:55 -0800

    Added getName()

--- src/me/dbizzzle/SkyrimRPG/Skill/Skill.java
+++ src/me/dbizzzle/SkyrimRPG/Skill/Skill.java
@@ -10,4 +10,16 @@ public enum Skill
 	ARCHERY, SWORDSMANSHIP, AXECRAFT, PICKPOCKETING, 
 	LOCKPICKING, BLOCKING,
 	CONJURATION, DESTRUCTION;
+	public String getName()
+	{
+		String sl = toString().toLowerCase().replaceAll("_", " ");
+		String[] st = sl.split("[ ]");
+		String m = Character.toUpperCase(st[0].charAt(0)) + st[0].substring(1);
+		for(String x: st)
+		{
+			if(x.equalsIgnoreCase(st[0]))continue;
+			m = m + " " + Character.toUpperCase(x.charAt(0)) + x.substring(1);
+		}
+		return m;
+	}
 }
