251d9d1819ff275d5aa88a322b4620fba0c9857f
Technius <bryan99tan@gmail.com>
2012-01-27 18:49:21 -0800

    Added a class for finding new versions

---
+++ src/me/dbizzzle/SkyrimRPG/VersionManager.java
@@ -0,0 +1,43 @@
+package me.dbizzzle.SkyrimRPG;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class VersionManager 
+{
+	private SkyrimRPG s;
+	public VersionManager(SkyrimRPG s)
+	{
+		this.s = s;
+	}
+	public String getLatestVersion()
+	{
+		String version = null;
+		URL u;
+		try {
+			u = new URL("https://raw.github.com/Technius/SkyrimRPG/master/src/plugin.yml");
+			URLConnection uc = u.openConnection();
+			BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			String l;
+			while((l=br.readLine()) != null)
+			{
+				String[] tokens = l.split("[:]");
+				if(tokens.length != 2)continue;
+				if(!tokens[0].equalsIgnoreCase("version"))continue;
+				version = tokens[1].trim();
+			}
+			br.close();
+		} 
+		catch (MalformedURLException e) 
+		{
+			return "NoHostFound";
+		} catch (IOException e) {
+			return null;
+		}
+		return version;
+	}
+}
